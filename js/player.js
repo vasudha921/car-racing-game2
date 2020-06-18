0574ff6f1368edb2f0567198b4966328c948296d
vasudha921 <shwethamoorthy@gmail.com>
2020-06-18 15:03:20 +0530

    updated a car game

--- js/player.js
+++ js/player.js
@@ -1,5 +1,9 @@
 class Player {
-    constructor(){}
+    constructor(){
+      this.index = null;
+      this.distance = 0;
+      this.name = null;
+    }
   
     getCount(){
       var playerCountRef = database.ref('playerCount');
@@ -15,9 +19,16 @@ class Player {
     }
   
     update(name){
-      var playerIndex = "player" + playerCount;
+      var playerIndex = "players/player" + this.index;
       database.ref(playerIndex).set({
-        name:name
+        name:this.name, 
+        distance:this.distance
       });
+    }
+    static getPlayerInfo (){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
     }
   }
--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var canvas, backgroundImage;
 var gameState = 0;
 var playerCount;
 var form, player, game;
-
+var allPlayers;
 
 function setup(){
   database = firebase.database();
@@ -15,6 +15,12 @@ function setup(){
 }
 
 function draw(){
-  
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
 }
 
